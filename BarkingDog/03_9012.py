cde5ec03196db184474e5b9ee6d7ce6ffae4b9d9
HyoJong <gywhd408@naver.com>
2022-04-01 17:15:41 +0900

    commit on 4.1

--- BarkingDog/03_9012.py
+++ BarkingDog/03_9012.py
@@ -2,14 +2,38 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
+n = int(input().rstrip())
 result = []
 for i in range(n):
     vps = input().rstrip()
 
     stack = []
-    top = 0
+    top = -1
+    check = True
     for v in vps:
-        if top < 1:
+        #print(stack)
+        if top < 0:
+            if v == ')':
+                check = False
+                break
+            stack.append(v)
+            top += 1
+            continue
 
-    print(vps)
+        if v == ')' and stack[top] == '(':
+            stack.pop()
+            top -= 1
+        else:
+            stack.append(v)
+            top += 1
+
+    if top > -1:
+        check = False
+
+    if check:
+        result.append('YES')
+    else:
+        result.append('NO')
+
+for r in result:
+    print(r)
