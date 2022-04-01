aa9eb94befd4dceb1c23548f0b12e95501417eaa
hjmoons <gywhd408@naver.com>
2022-04-01 15:47:56 +0900

    commit on 4.1

---
+++ BarkingDog/03_9012.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+result = []
+for i in range(n):
+    vps = input().rstrip()
+
+    stack = []
+    top = 0
+    for v in vps:
+        if top < 1:
+
+    print(vps)
