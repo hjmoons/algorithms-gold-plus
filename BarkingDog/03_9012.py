039ee9fd3fb9fba510c640eb5b1820d4dfc4d171
HyoJong <gywhd408@naver.com>
2022-04-01 17:19:20 +0900

    commit on 4.1

--- BarkingDog/03_9012.py
+++ BarkingDog/03_9012.py
@@ -11,8 +11,9 @@ for i in range(n):
     top = -1
     check = True
     for v in vps:
-        #print(stack)
+        # 스택에 값이 없는 경우
         if top < 0:
+            # ')'가 시작이면 VPS가 아닌 경우
             if v == ')':
                 check = False
                 break
@@ -20,6 +21,9 @@ for i in range(n):
             top += 1
             continue
 
+        # 스택의 탑과 비교하려는 대상이 '(', ')' 경우
+        # VPS 일치하므로 팝 연산 수행
+        # 일치하지 않을경우 스택에 푸쉬
         if v == ')' and stack[top] == '(':
             stack.pop()
             top -= 1
@@ -27,6 +31,7 @@ for i in range(n):
             stack.append(v)
             top += 1
 
+    # 스택에 괄호가 남으면 VPS가 아닌 경우
     if top > -1:
         check = False
 
