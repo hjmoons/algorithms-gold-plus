c361ab60265a6ce884e9e8ce7c0f7ea6e88766ab
HyoJong <gywhd408@naver.com>
2022-04-07 23:28:07 +0900

    commit on 4.7

--- PikedBackjoon/0410_delivery_5972.py
+++ PikedBackjoon/0410_delivery_5972.py
@@ -1,3 +1,13 @@
 import sys
+from collections import defaultdict
 
 input = sys.stdin.readline
+
+n, m = map(int, input().rstrip().split())
+road = defaultdict(dict)
+
+for _ in range(8):
+    a, b, c = map(int, input().rstrip().split())
+    road[a][b] = c
+
+print(road)
