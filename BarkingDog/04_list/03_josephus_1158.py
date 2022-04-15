382dc1c8db8c0ea023bdb5aaa7f44a65197fa376
HyoJong <gywhd408@naver.com>
2022-04-15 18:02:33 +0900

    unsolved top

--- BarkingDog/04_list/03_josephus_1158.py
+++ BarkingDog/04_list/03_josephus_1158.py
@@ -7,14 +7,14 @@ circle = [i + 1 for i in range(n)]
 result = []
 index = 0       # 현재 인덱스
 
-while circle:
-    index += k - 1
+while circle:       # 원에 수가 존재하지 않을 때까지
+    index += k - 1  # 시작 index에서 k-1을 더한 위치가 제거할 수가 있는 위치
 
-    if index > len(circle) - 1:
-        index %= len(circle)
+    if index > len(circle) - 1:     # index가 현재 원에 있는 수의 개수를 넘어갈 경우
+        index %= len(circle)        # 원의 맨 처음 index로 다시 돌아감
 
-    result.append(circle[index])
-    del circle[index]
+    result.append(circle[index])    # 해당 index를 결과에 추가
+    del circle[index]               # del 연산을 통해 수를 제거하고 뒤에 있는 수들을 앞으로 당김 O(n)
 
 print("<" + str(result)[1:-1] + ">")
 
--- BarkingDog/05_stack/02_zero_10773.py
+++ BarkingDog/05_stack/02_zero_10773.py
@@ -7,11 +7,12 @@ money = []
 
 for _ in range(k):
     n = int(input().rstrip())
-    if n == 0:
+    if n == 0:              # 0 일경우 돈 리스트에서 팝
         money.pop()
-    else:
+    else:                   # 그 외에는 다 푸쉬
         money.append(n)
 
+# 리스트의 있는 수의 총합
 sum = 0
 for m in money:
     sum += m
