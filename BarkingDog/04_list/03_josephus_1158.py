4ae8c88816c2e88627c2267061e1598c3d0c2777
HyoJong <gywhd408@naver.com>
2022-04-15 16:50:10 +0900

    solved josephus

--- BarkingDog/04_list/03_josephus_1158.py
+++ BarkingDog/04_list/03_josephus_1158.py
@@ -1,10 +1,26 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 n, k = map(int, input().strip().split())
 
-'''
+circle = [i + 1 for i in range(n)]
+result = []
+index = 0       # 현재 인덱스
+
+while circle:
+    index += k - 1
+
+    if index > len(circle) - 1:
+        index %= len(circle)
+
+    result.append(circle[index])
+    del circle[index]
+
+print("<" + str(result)[1:-1] + ">")
+
+''' 시간초과
+from collections import deque
+
 circle = deque([i + 1 for i in range(n)])
 result = []
 index = 0
@@ -17,15 +33,3 @@ while circle:
         circle.append(circle.popleft())
         index += 1
 '''
-
-circle = [i + 1 for i in range(n)]
-result = []
-index = 0
-
-while circle:
-    if index == k - 1:
-        del circle[index]
-
-
-
-print("<" + str(result)[1:-1] + ">")
