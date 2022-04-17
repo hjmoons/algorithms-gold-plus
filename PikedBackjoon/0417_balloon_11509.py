69080194b6b746fe964dd9b53c6335f36455874c
HyoJong <gywhd408@naver.com>
2022-04-17 23:38:17 +0900

    solved balloon

--- PikedBackjoon/0417_balloon_11509.py
+++ PikedBackjoon/0417_balloon_11509.py
@@ -5,20 +5,18 @@ input = sys.stdin.readline
 n = int(input().rstrip())
 balloons = list(map(int, input().rstrip().split()))
 
-check = [0] * 1000001   # 화살의 위치
+check = [0] * 1000001   # 화살의 위치 ( n+1로 했는데 런타임 에러 발생 -> 1000001로 변경)
 count = 0               # 화살 수
 
 for i in range(n):
     bal = balloons[i]
 
     # 현재 풍선 높이에 화살이 있는지 확인
-    # 화살이 없는 경우 (0인 경우) -> 새로운 화살 발사, 현재 풍선 터뜨리고 1을 뺀 높이에 화살 표시
-    # 화살이 있는 경우 (0이 아닌 경우) -> 현재 풍선을 터뜨리고 화살을 없앤 후(-1) 1을 뺀 높이에 화살 표시
-    if check[bal] == 0:
-        count += 1
-        check[bal - 1] += 1
-    else:
-        check[bal] -= 1
-        check[bal - 1] += 1
+    if check[bal] == 0:         # 화살이 없는 경우 (0인 경우) -> ,
+        count += 1              # 새로운 화살 발사
+        check[bal - 1] += 1     # 현재 풍선 터뜨리고 1을 뺀 높이에 화살 표시
+    else:                       # 화살이 있는 경우 (0이 아닌 경우) ->
+        check[bal] -= 1         # 현재 풍선을 터뜨리고 화살 이동
+        check[bal - 1] += 1     # 화살의 높이를 1 줄인 위치에 화살 표시
 
 print(count)
