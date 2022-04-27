95a567f5d86a97554df3079ad5cc2f6e72e3fba8
hjmoons <gywhd408@naver.com>
2022-04-27 17:09:14 +0900

    unsolved lcs problem

--- PikedBackjoon/0424_lcs_9251.py
+++ PikedBackjoon/0424_lcs_9251.py
@@ -5,26 +5,34 @@ input = sys.stdin.readline
 str1 = input().rstrip()
 str2 = input().rstrip()
 
-dp = [[0 for _ in range(len(str1))] for _ in range(len(str2))]
+lcs = [[0 for _ in range(len(str2))] for _ in range(len(str1))]
 
 for i in range(len(str1)):
     for j in range(len(str2)):
         if str1[i] == str2[j]:
-            dp[i][j] += 1
+            lcs[i][j] += 1
 
-for d in dp:
+for d in lcs:
     print(d)
+print()
 
-for i in range(1, len(str1)):
+dp = [0] * len(lcs[0])
+for i in range(len(str1)):
     count = 0
-    print(dp[0], i)
+    print(dp)
+    print(lcs[i])
+    print()
+
     for j in range(len(str2)):
-        if j == 0 and dp[i][j] == 1:
-            dp[0][j] = 1
-        #elif dp[0][j] >= 1:
-            #if dp[i][j] == 1:
-            #count = dp[i][j]
+        if j == 0 and lcs[i][j] == 1:
+            dp[j] = 1
+            count = 1
+        else:
+            if dp[j-1] > count:
+                count = dp[j-1]
 
-        count = dp[i][j]
+            if lcs[i][j] == 1:
+                dp[j] = count + 1
 
-print(dp[0])
+print(dp)
+print(max(dp))
